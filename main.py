9b1add73f48fc0b37e0d98448f79d20ed74f71f5
Zachary Kaye <zacharyckaye@gmail.com>
2022-03-09 21:28:02 +1300

    Severities vs year for distinct packages

--- main.py
+++ main.py
@@ -2,45 +2,77 @@ import json
 import csv
 from os import path
 from helpers import get_list_of_files
+from severity import Severity
+
+
+def valid_vulnerability(data):
+    return len(data['affected']) > 0 and not withdrawn(data)
 
 
 def withdrawn(data):
     return 'withdrawn' in data
 
 
-def valid_vulnerability(data):
-    return len(data['affected']) > 0
+def get_new_affected_packages(data, packages):
+    for package in data['affected']:
+        package_name = package['package']['name']
+        year_vulnerable = get_year_discovered(data)
+        if package_name in packages.keys():
+            if year_vulnerable in packages[package_name].keys():
+                new_severity = get_severity(data, packages[package_name][year_vulnerable])
+            else:
+                new_severity = get_severity(data)
+            packages[package_name][year_vulnerable] = new_severity
+        else:
+            packages[package_name] = dict()
+            packages[package_name][year_vulnerable] = get_severity(data)
+    return packages
+
+
+def get_year_discovered(data):
+    if len(data['aliases']) > 0:
+        return data['aliases'][0].split('-')[1]
+    return data['published'].split('-')[0]
+
+
+def get_severity(data, current_severity=Severity.LOW.name):
+    data_severity = data['database_specific']['severity']
+    if Severity[data_severity].value > Severity[current_severity].value:
+        return Severity[data_severity].name
+    return current_severity
 
 
 file_path = path.join(*'data/advisory-database/advisories/github-reviewed'.split('/'))
 files = get_list_of_files(file_path)
 
-output_file = open("vulnerabilities.csv", "w", newline='')
+output_file = open("output.csv", "w", newline='')
 writer = csv.writer(output_file)
 
 output_data = dict()
+affected_packages = dict()
 
 for file in files:
     input_file = open(file, 'r', encoding='utf-8')
-    json_data = json.load(input_file)
-    if valid_vulnerability(json_data) and not withdrawn(json_data):
-        year_discovered = json_data['aliases'][0].split('-')[1] if len(json_data['aliases']) > 0 else json_data['published'].split('-')[0]
-        severity = json_data['database_specific']['severity']
+    vulnerability = json.load(input_file)
+    if valid_vulnerability(vulnerability):
+        affected_packages = get_new_affected_packages(vulnerability, affected_packages)
+
+for affected_package in affected_packages.values():
+    for year_discovered, severity in affected_package.items():
         if year_discovered in output_data:
             if severity in output_data[year_discovered]:
                 output_data[year_discovered][severity] += 1
             else:
                 output_data[year_discovered][severity] = 1
         else:
-            output_data[year_discovered] = dict()
+            output_data[year_discovered] = {'LOW': 0, 'MODERATE': 0, 'HIGH': 0, 'CRITICAL': 0}
             output_data[year_discovered][severity] = 1
+
 
 for year, severities in output_data.items():
     row = [year]
-    for severity, number_vulnerabilities in severities.items():
-        row.append((severity, number_vulnerabilities))
+    for severity in severities:
+        row.append(severities[severity])
     writer.writerow(row)
 
-
 output_file.close()
-
---
+++ severity.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Severity(Enum):
+    LOW = 1
+    MODERATE = 2
+    HIGH = 3
+    CRITICAL = 4
