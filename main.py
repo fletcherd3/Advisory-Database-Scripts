e0ccb40bf60405557182ebef077cecb5945ae7ca
Zachary Kaye <zacharyckaye@gmail.com>
2022-03-09 21:49:03 +1300

    Managers vs year for distinct packages

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import json
 import csv
 from os import path
 from helpers import get_list_of_files
-from severity import Severity
 
 
 def valid_vulnerability(data):
@@ -17,15 +16,9 @@ def get_new_affected_packages(data, packages):
     for package in data['affected']:
         package_name = package['package']['name']
         year_vulnerable = get_year_discovered(data)
-        if package_name in packages.keys():
-            if year_vulnerable in packages[package_name].keys():
-                new_severity = get_severity(data, packages[package_name][year_vulnerable])
-            else:
-                new_severity = get_severity(data)
-            packages[package_name][year_vulnerable] = new_severity
-        else:
+        if package_name not in packages.keys():
             packages[package_name] = dict()
-            packages[package_name][year_vulnerable] = get_severity(data)
+        packages[package_name][year_vulnerable] = package['package']['ecosystem']
     return packages
 
 
@@ -33,13 +26,6 @@ def get_year_discovered(data):
     if len(data['aliases']) > 0:
         return data['aliases'][0].split('-')[1]
     return data['published'].split('-')[0]
-
-
-def get_severity(data, current_severity=Severity.LOW.name):
-    data_severity = data['database_specific']['severity']
-    if Severity[data_severity].value > Severity[current_severity].value:
-        return Severity[data_severity].name
-    return current_severity
 
 
 file_path = path.join(*'data/advisory-database/advisories/github-reviewed'.split('/'))
@@ -58,21 +44,22 @@ for file in files:
         affected_packages = get_new_affected_packages(vulnerability, affected_packages)
 
 for affected_package in affected_packages.values():
-    for year_discovered, severity in affected_package.items():
+    for year_discovered, package_manager in affected_package.items():
         if year_discovered in output_data:
-            if severity in output_data[year_discovered]:
-                output_data[year_discovered][severity] += 1
+            if package_manager in output_data[year_discovered]:
+                output_data[year_discovered][package_manager] += 1
             else:
-                output_data[year_discovered][severity] = 1
+                output_data[year_discovered][package_manager] = 1
         else:
-            output_data[year_discovered] = {'LOW': 0, 'MODERATE': 0, 'HIGH': 0, 'CRITICAL': 0}
-            output_data[year_discovered][severity] = 1
+            output_data[year_discovered] = {'RubyGems': 0, 'npm': 0, 'PyPI': 0, 'Maven': 0, 'NuGet': 0, 'Packagist': 0,
+                                            'Go': 0, 'crates.io': 0}
+            output_data[year_discovered][package_manager] = 1
 
 
-for year, severities in output_data.items():
+for year, package_manager in output_data.items():
     row = [year]
-    for severity in severities:
-        row.append(severities[severity])
+    for manager in package_manager:
+        row.append(package_manager[manager])
     writer.writerow(row)
 
 output_file.close()
--- severity.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class Severity(Enum):
-    LOW = 1
-    MODERATE = 2
-    HIGH = 3
-    CRITICAL = 4
